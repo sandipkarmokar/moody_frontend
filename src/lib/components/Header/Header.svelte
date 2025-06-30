06aa7cd3b38e3e22757333989b7545a85c102fa3
sandipkarmokar <skarmokar987@gmail.com>
2025-07-01 00:26:39 +0530

    Refactor Header component layout and styling for improved design

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 </script>
 
-<section class="bg-lavender-dark w-full">
-	<div class="flex h-24 items-center justify-between">
-		<p>Moody</p>
+<section
+	class="bg-lavender-dark mt-12 flex h-20 w-full items-center justify-center rounded-full shadow-xs"
+>
+	<div class="flex w-[84%] items-center justify-between text-slate-600">
+		<p class="text-xl font-bold">Moody</p>
 		<p>userName</p>
 	</div>
 </section>
