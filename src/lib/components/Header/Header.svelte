a901f667b059efb47a42b8a76f2ab4f8527362f4
sandipkarmokar <skarmokar987@gmail.com>
2025-07-01 00:28:04 +0530

    Update Header component to adjust width for better layout consistency

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -4,7 +4,7 @@
 <section
 	class="bg-lavender-dark mt-12 flex h-20 w-full items-center justify-center rounded-full shadow-xs"
 >
-	<div class="flex w-[84%] items-center justify-between text-slate-600">
+	<div class="flex w-[90%] items-center justify-between text-slate-600">
 		<p class="text-xl font-bold">Moody</p>
 		<p>userName</p>
 	</div>
